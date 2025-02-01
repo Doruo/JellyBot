7c3304a542f78135517dda1eaca481504fa100fe
marchaye <marc.antonio.haye@gmail.com>
2025-02-01 19:48:48 +0100

    refactor: compactage code main

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from src.bot import JellyfinBot
 from config.globals import GlobalConfig
 
 print("Validating global config...")
-
 GlobalConfig.validate_config(GlobalConfig)
 
 print("Importing global values...")
-
 # Jellyfin
 JELLYFIN_HOST = GlobalConfig.JELLYFIN_HOST
 JELLYFIN_PORT = GlobalConfig.JELLYFIN_PORT
@@ -19,23 +17,15 @@ DISCORD_CHANNEL_ID = GlobalConfig.DISCORD_CHANNEL_ID
 DISCORD_TOKEN = GlobalConfig.DISCORD_TOKEN
 
 print("Starting bot...")
-
 bot = JellyfinBot(f'{JELLYFIN_HOST}:{JELLYFIN_PORT}', JELLYFIN_API_KEY, ADMIN_USER_ID,DISCORD_CHANNEL_ID)
 
-print("Binding discord commands...")
-
-# /-/-/-/-/ COMMANDS /-/-/-/-/
-
+print("Binding Discord commands...")
 @bot.command(name="state")
 async def state(ctx):
     await bot.state(ctx)
-
 @bot.command(name="latest")
 async def latest(ctx):
     await bot.latest(ctx)
 
-# /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
-
 print("Connecting to Discord...")
-
 bot.run(DISCORD_TOKEN)
