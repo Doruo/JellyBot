595a46b5dff25e8a6241622b68d0beca08177910
marchaye <marc.antonio.haye@gmail.com>
2025-02-09 16:54:02 +0100

    feat: installer fonctionnel

--- scripts/installer.py
+++ scripts/installer.py
@@ -26,87 +26,95 @@ def confirm(message: str = "Continue", default: bool | None = None, direct: bool
         return None
     return output
 
-# INSTALLATION START
-
-# OS CONFIG DETECTION
-"""
-    if platform.system() != "Windows":
-        if os.environ.get("XDG_CONFIG_HOME"):
-            path = os.environ["XDG_CONFIG_HOME"].removesuffix("/") + "/JellyBot/"
-        else:
-            path = os.environ["HOME"].removesuffix("/") + "/.config/JellyBot/"
-
-        subprocess.run(["mkdir", "-p", path])
-    else:
-        path = os.environ["APPDATA"].removesuffix("\\") + "\\JellyBot\\"
-        subprocess.run(
-            ["powershell", "-Command", f'mkdir "{path}"'],
-            stdout=subprocess.DEVNULL,
-        )
-"""
-
-print("""
-Welcome to the "JellyBot" Jellyfin Discord Bot installer
-More docs at: [https://github.com/Doruo/JellyBot#installation]
-""")
-
-print("----------Jellyfin----------")
-
-print("bash")
-
-protocol = input("http/https: ")
-host = input("hostname: ")
-port = input("port: ")
-url = f'{protocol}://{host}:{port}'
-
-print(f"url: {url}")
-
-print("Enter a single username or enter multiple usernames in a comma separated list.")
-username = input("username[s]: ").split(",")
-
-self_signed_cert = None
-
-if protocol == "https":
-    self_signed_cert = confirm(
-        message="Are you using a self signed certificate?", default=False, direct=True
-        )
-
-api_key = input(f"API key [Create one here: {url}/web/index.html#!/apikeys.html]: ")
-
-print ("To find your admin user id : connect to your jellyfin website as Admin > Left Panel > Users > your user, and copy the user Id from the url")
-admin_user_id = input("Admin User ID: ")
-
-print("----------Discord----------")
-
-app_id = input("Application ID: ")
-discord_token = input("Discord Token: ")
-discord_channel_id = input("Discord Channel ID: ")
-   
-# Get the directory where the current script is located (scripts folder)
-current_script_path = Path(__file__).resolve()
-scripts_dir = current_script_path.parent
+# Input environnement
+def get_env_vars_input():
     
-# Create .env file directly in scripts directory
-path = scripts_dir / ".env"
+    print("""
+    Welcome to the JellyBot - Jellyfin Discord Bot installer
+    More docs at: [https://github.com/Doruo/JellyBot#installation]
+    """)
 
-# Try creating .env with environment variables given in the correct folder
-try:
-    with open(path, "w") as file:
+    print("----------Jellyfin----------")
+
+    protocol = input("Protocol (http/https): ")
+    host = input("Hostame: ")
+    port = input("Port: ")
+    url = f'{protocol}://{host}:{port}'
+
+    print(f"Jellyfin Server url: {url}")
+
+    print("Enter a single username or enter multiple usernames in a comma separated list.")
+    username = input("Username[s]: ").split(",")
+
+    api_key = input(f"API key [Create one here: {url}/web/index.html#!/apikeys.html]: ")
+
+    print ("To find your admin user id : connect to your jellyfin website as Admin > Left Panel > Users > your user, and copy the user Id from the url")
+    admin_user_id = input("Admin User ID: ")
+
+    print("----------Discord----------")
+
+    app_id = input("Application ID: ")
+    discord_token = input("Discord Token: ")
+    discord_channel_id = input("Discord Channel ID: ")
+
+    env_var = set(
+        protocol,host,port,url,username,api_key,admin_user_id,
+        app_id,discord_token,discord_channel_id
+        )
+    
+    return env_var
+
+# Configure environnement from parameters
+def create_env_file(env_var: set):
+    
+    # Get the directory where the current script is located (scripts folder)
+    current_script_path = Path(__file__).resolve()
+    project_root = current_script_path.parent.parent
         
-        # write environment variables
-        file.write("# DISCORD\n")
-        file.write(f"APPLICATION_ID={app_id}\n")
-        file.write(f"DISCORD_TOKEN={discord_token}\n")
-        file.write(f"DISCORD_CHANNEL_ID={discord_channel_id}\n")
+    # Create config directory if it doesn't exist
+    config_dir = project_root / "config"
+    config_dir.mkdir(exist_ok=True)
 
-        file.write("# JELLYFIN\n")
-        file.write(f"JELLYFIN_PROTOCOL={protocol}\n")
-        file.write(f"JELLYFIN_HOST={host}\n")
-        file.write(f"JELLYFIN_PORT={port}\n")
-        file.write(f"JELLYFIN_API_KEY={app_id}\n")
-        file.write(f"ADMIN_USER_ID={app_id}\n")
-    
-    print(f"Successfully created .env file at: {path}")
-    
-except Exception as e:
-    print(f"Error creating .env file: {e}")
+    # Create .env file in config directory
+    env_path = config_dir / ".env"
+
+    # Try creating .env with environment variables given in the correct folder
+    try:
+        with open(env_path, "w") as file:
+            
+            # write environment variables
+            # Jellyfin environment variables
+            file.write("# JELLYFIN\n")
+
+            # Jellyfin server url
+            file.write(f"JELLYFIN_PROTOCOL={env_var['protocol']}\n")
+            file.write(f"JELLYFIN_HOST={env_var['host']}\n")
+            file.write(f"JELLYFIN_PORT={env_var['port']}\n")
+
+            # Jellyfin API
+            file.write(f"JELLYFIN_API_KEY={env_var['api_key']}\n")
+
+            # Jellyfin admin user
+            file.write(f"ADMIN_USER_ID={env_var['admin_user_id']}\n")
+            file.write(f"ADMIN_USERNAME={env_var['username']}\n")
+
+            # Discord environment variables
+            file.write("# DISCORD\n")
+
+            # Discord Bot Application
+            file.write(f"APPLICATION_ID={env_var['app_id']}\n")
+            file.write(f"DISCORD_TOKEN={env_var['discord_token']}\n")
+
+            # Discord Text Channel ID
+            file.write(f"DISCORD_CHANNEL_ID={env_var['discord_channel_id']}\n")
+        
+        print(f"Successfully created .env file at: {env_path}")
+        
+    except Exception as e:
+        print(f"Error creating .env file: {e}")
+
+def install() :
+    env_var = get_env_vars_input()
+    create_env_file(env_var)
+
+install()
